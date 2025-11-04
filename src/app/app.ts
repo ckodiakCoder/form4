8b1d72af1a2cc52ea2342d08a3af0aa8a0edfe5a
Nigam Kumar Shu <nigamidea@gmail.com>
2025-11-04 15:09:09 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
